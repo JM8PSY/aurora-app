81712da2665646d09c18dd6b5925979cc7a5fde3
jm8psy <jm8psy.qso@gmail.com>
2025-06-06 18:18:16 +0900

    NOAAのKpを取得することに成功

--- backend/app/api.py
+++ backend/app/api.py
@@ -12,18 +12,33 @@ def get_conditions(latitude: float, longitude: float):
     OPENWEATHER_API_KEY = os.getenv("OPENWEATHER_API_KEY")
     if not OPENWEATHER_API_KEY:
         raise RuntimeError("OPENWEATHER_API_KEY is not set in environment variables")
+    
     # OpenWeatherMap APIを呼び出して天候情報を取得
     url = f"https://api.openweathermap.org/data/2.5/weather?lat={latitude}&lon={longitude}&appid={OPENWEATHER_API_KEY}&units=metric&lang=ja"
     print(f"Fetching weather data from: {url}")
+
     response = requests.get(url) # API呼び出し
     if response.status_code != 200: # ステータスコードが200でない場合はエラー
         raise RuntimeError(f"Failed to fetch data from OpenWeatherMap: {response.status_code} {response.text}")
+    
     data = response.json()
     print(f"Received data: {json.dumps(data, ensure_ascii=False, indent=2)}")
-    
+
+    # NOAAのKp指数
+    response_kp = requests.get("https://services.swpc.noaa.gov/products/noaa-planetary-k-index.json")
+    if response_kp.status_code != 200:
+        raise RuntimeError(f"Failed to fetch Kp index from NOAA: {response_kp.status_code} {response_kp.text}")
+    kp_data = response_kp.json()
+    print(f"Received Kp index data: {json.dumps(kp_data, ensure_ascii=False, indent=2)}")
+    # 最新のKp値を取得
+    if len(kp_data) > 1:
+        latest_kp = float(kp_data[-1][1])
+    else:
+        latest_kp = None
+
     return ObservationConditions(
         cloud_cover= data.get("clouds", {}).get("all", 0), # 雲量（%）
-        kp_index=6.0, # 仮の値
+        kp_index_fromNOAA=latest_kp, # 仮の値
         relative_humidity=  data.get("main", {}).get("humidity", 0),# 相対湿度
         visibility_north=True,
         recommendation="北の空が晴れていれば観測チャンス！"
--- backend/app/models.py
+++ backend/app/models.py
@@ -2,7 +2,7 @@ from pydantic import BaseModel
 
 class ObservationConditions(BaseModel):
     cloud_cover: int            # 雲量（%）
-    kp_index: float             # オーロラ指数
-    relative_humidity: int        # 相対湿度
+    kp_index_fromNOAA: int      # NOAAからのKp指数
+    relative_humidity: int      # 相対湿度
     visibility_north: bool      # 北側の空が開けているか
     recommendation: str         # 一言アドバイス
